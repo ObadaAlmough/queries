dcbfc9d1726be9836a6a05bdf6ab876149fa281e
obadaAlmoug <83464909+obadaAlmoug@users.noreply.github.com>
2021-09-02 03:51:42 +0200

    task

---
+++ queries.sql
@@ -0,0 +1,30 @@
+-- 1- from “orderdetails” table. Write a query to get all order numbers (without repetition) that contain any product starting with code S18 and price more than 100.
+
+SELECT DISTINCT productCode FROM orderdetails
+WHERE productCode LIKE 's18%'
+AND priceEach > 100;
+
+-- 2- from “payments” table. Write a query to get all payments that were made on day 5 or 6 (whatever month or year).
+
+
+SELECT * FROM `payments`
+WHERE paymentDate LIKE '_______-05'
+OR paymentDate LIKE '_______-06';
+
+-- 3- from “customers” table. Write a query to get the fifth-highest credit limit of the customers who (live in the USA and their phone number contains 5555).
+
+SELECT * FROM `customers`
+WHERE country = 'usa'
+AND phone LIKE '%5555%'
+order BY creditLimit DESC
+LIMIT 1 OFFSET 5;
+
+
+
+
+
+
+
+
+
+
